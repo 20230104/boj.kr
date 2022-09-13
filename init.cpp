464c022c1c70071237ea15fe2f4951c1a808c02c
javaboooom <58615028+javaboooom@users.noreply.github.com>
2022-09-13 13:34:50 +0000

    init.cpp

---
+++ init.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class my
+{
+private:
+public:
+my(){
+}
+void Ution(){
+}
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    my sol;
+    sol.Ution();
+    
+    return 0;
+}
